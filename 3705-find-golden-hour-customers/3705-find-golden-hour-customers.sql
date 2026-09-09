520c9e05b8529e656811ce4ef80f4eba8b0ea097
AADITYA1610 <144117064+AADITYA1610@users.noreply.github.com>
2026-09-09 21:41:59 +0530

    Time: 306 ms (80.85%), Space: 0B (100%) - LeetHub

---
+++ 3705-find-golden-hour-customers/3705-find-golden-hour-customers.sql
@@ -0,0 +1,34 @@
+SELECT
+    customer_id,
+    COUNT(*) AS total_orders,
+    ROUND(
+        100.0 * SUM(
+            CASE
+                WHEN TIME(order_timestamp) BETWEEN '11:00:00' AND '14:00:00'
+                  OR TIME(order_timestamp) BETWEEN '18:00:00' AND '21:00:00'
+                THEN 1
+                ELSE 0
+            END
+        ) / COUNT(*)
+    ) AS peak_hour_percentage,
+    ROUND(AVG(order_rating), 2) AS average_rating
+FROM restaurant_orders
+GROUP BY customer_id
+HAVING
+    COUNT(*) >= 3
+    AND
+    SUM(
+        CASE
+            WHEN TIME(order_timestamp) BETWEEN '11:00:00' AND '14:00:00'
+              OR TIME(order_timestamp) BETWEEN '18:00:00' AND '21:00:00'
+            THEN 1
+            ELSE 0
+        END
+    ) / COUNT(*) >= 0.60
+    AND
+    COUNT(order_rating) / COUNT(*) >= 0.50
+    AND
+    AVG(order_rating) >= 4.0
+ORDER BY
+    average_rating DESC,
+    customer_id DESC;
